17b29fa36451164598760c02fe6454b1e7fc5ed1
j.montoya <j.montoya@bbva.com>
2022-04-28 19:56:54 -0500

    feat: ejercicio de acceso a arreglos y objetos

--- 1-logica/1.4-codigo/calculadora-matrices/src/index.js
+++ 1-logica/1.4-codigo/calculadora-matrices/src/index.js
@@ -7,7 +7,7 @@ var student = {
     name: 'juanito lopez',
     age: 29,
     isMarried: true,
-    matters: [10, 8, 4, 5, 2, 1]
+    matters: [10, 8, 4, 5, 2, 1, 67, 42, 99]
 }
 var computer = {
     brand: 'dell',
@@ -16,10 +16,12 @@ var computer = {
 };
 let numbers = [10, 20, 30, 40];
 var subjects = ['Math', 'English', 'History', 'English', 'Computer science', 'English', 'History'];
-let gradesArr = [10, 9, 0, { value: 10, nota: "excelencia academica"} ];
+let gradesArr = [true, 10, 9, 0, { value: 10, nota: "excelencia academica"} ];
 let matriz = [
-    [10, 10, 9],
-    [9, 9 , 8, 100, 7, 10]
+    [0, 10, 9],
+    [9, 9 , 8, 100, 7, 10, 10, 10, 9, 10, 10, 9],
+    [9, 9 , 8, 100, 7, 10, 10, 1120, 9],
+    [9, 9 , 8, 100, 777, 10],
 ];
 
 console.log(name);
@@ -31,5 +33,15 @@ console.log(gradesArr[3].nota);
 console.log(student.matters[4]);
 console.log(matriz[1][3]);
 
-
-
+// imprimir la edad del student
+console.log();
+// imprimir el valor 67 de las matters de student
+console.log();
+// imprimir el valor 1120 de la matriz
+console.log();
+// imprimir el valor 777 de la matriz
+console.log();
+// imprimir el valor 0 de la matriz
+console.log();
+// imprimir el valor true de gradesArr
+console.log();
