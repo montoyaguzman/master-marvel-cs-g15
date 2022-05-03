c677d188cf90d43421beea802ca66e4763f520bf
j.montoya <j.montoya@bbva.com>
2022-05-02 19:12:42 -0500

    chore: avance de la multiplicacion

--- 1-logica/1.4-codigo/calculadora-matrices/src/index.js
+++ 1-logica/1.4-codigo/calculadora-matrices/src/index.js
@@ -22,6 +22,12 @@ const matrizResultado2 = [
     [0, 0, 0]
 ];
 
+const matrizResultado3 = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
 console.log('matrizA => ', matrizA);
 console.log('matrizB => ', matrizB);
 
@@ -32,6 +38,9 @@ console.log('matrizB => ', matrizB);
 // console.log(matrizB[1][1]);
 // console.log(matriz[0][3]); // undefined
 
+/**
+ * SUMA
+ */
 for (var fila = 0; fila < matrizResultado.length; fila++) {
     for (var columna = 0; columna < matrizResultado.length; columna++) {
         // debugger
@@ -41,14 +50,38 @@ for (var fila = 0; fila < matrizResultado.length; fila++) {
     }
 }
 
+/**
+ * RESTA
+ */
 for (var fila = 0; fila < 3; fila++) {
     for (var columna = 0; columna < 3; columna++) {
         matrizResultado2[fila][columna] = matrizA[fila][columna] - matrizB[fila][columna]
     }
 }
 
+/** 
+ * Reglas de la MULTIPLICACION de matrices
+ * 1. matrizResultado[filasM1][columnasM2] 
+ * 2. Solo se puede multiplicar M1 y M2 si columnasM1 === filasM2
+*/
+for (var fila = 0; fila < matrizResultado3.length; fila++) {
+    for (var columna = 0; columna < matrizResultado3.length; columna++) {
+        matrizResultado3[fila][columna] = matrizResultado3[fila][columna] + matrizA[fila][columna] * matrizB[fila][columna]
+        // matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][0] * matrizB[0][0]
+        // matrizResultado3[1][1] = matrizResultado3[1][1] + matrizA[1][1] * matrizB[1][1]
+
+
+        matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][0] * matrizB[0][0]
+        matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][1] * matrizB[1][0]
+        matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][2] * matrizB[2][0]
+
+
+    }   
+}
+
 console.log('matrizResultado (suma) => ', matrizResultado);
 console.log('matrizResultado (resta) => ', matrizResultado2);
+console.log('matrizResultado (matriz) => ', matrizResultado3);
 console.log(matrizA.length)
 
 // < Menor
