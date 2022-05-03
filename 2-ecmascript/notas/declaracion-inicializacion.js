c7a444fdce5f09c70a18410651363b7e7ec7eb38
j.montoya <j.montoya@bbva.com>
2022-05-02 20:52:34 -0500

    feat: notas de declaracion de inicializacion

---
+++ 2-ecmascript/notas/declaracion-inicializacion.js
@@ -0,0 +1,21 @@
+// declaracion
+var school;
+// inicializacion
+school = "Dev.f";
+
+// inicializar y declarar
+var matter = "computer science";
+
+let age = 18;
+
+// declaracion
+let genre;
+console.log("genre => " + genre);
+// inicializacion
+genre = "Female";
+// inicializar y declarar
+let height = 1.87
+
+// inicializar y declarar
+const PI = 3.1416;
+// console.log("PI" + PI);
