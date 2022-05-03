57201c17274ac67d5401b820d76402f279e37b16
j.montoya <j.montoya@bbva.com>
2022-05-02 19:25:52 -0500

    feat: resuelve multiplicacion de matrices

--- 1-logica/1.4-codigo/calculadora-matrices/src/index.js
+++ 1-logica/1.4-codigo/calculadora-matrices/src/index.js
@@ -59,6 +59,18 @@ for (var fila = 0; fila < 3; fila++) {
     }
 }
 
+
+const matrizA2 = [
+    [1, 6, 0],
+    [-1, 3, 1],
+    [4, 6, 2]
+];
+
+const matrizB2 = [
+    [0, 4, 0],
+    [2, 3, 1],
+    [1, -2, 1]
+];
 /** 
  * Reglas de la MULTIPLICACION de matrices
  * 1. matrizResultado[filasM1][columnasM2] 
@@ -66,22 +78,35 @@ for (var fila = 0; fila < 3; fila++) {
 */
 for (var fila = 0; fila < matrizResultado3.length; fila++) {
     for (var columna = 0; columna < matrizResultado3.length; columna++) {
-        matrizResultado3[fila][columna] = matrizResultado3[fila][columna] + matrizA[fila][columna] * matrizB[fila][columna]
+        
+        /**
+         * Primer avance, tenia errores
+         */
+        // matrizResultado3[fila][columna] = matrizResultado3[fila][columna] + matrizA2[fila][columna] * matrizB2[fila][columna]
         // matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][0] * matrizB[0][0]
         // matrizResultado3[1][1] = matrizResultado3[1][1] + matrizA[1][1] * matrizB[1][1]
 
+        /**
+         * Pseudocodigo de correcion de errores
+         */
+        // matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][0] * matrizB[0][0]
+        // matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][1] * matrizB[1][0]
+        // matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][2] * matrizB[2][0]
 
-        matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][0] * matrizB[0][0]
-        matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][1] * matrizB[1][0]
-        matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][2] * matrizB[2][0]
-
+        /** Solucion */
+        for (var aux = 0; aux < matrizResultado3.length; aux++) {
+            matrizResultado3[fila][columna] = matrizResultado3[fila][columna] + matrizA2[fila][aux] * matrizB2[aux][columna];
+        }
 
     }   
 }
 
 console.log('matrizResultado (suma) => ', matrizResultado);
 console.log('matrizResultado (resta) => ', matrizResultado2);
-console.log('matrizResultado (matriz) => ', matrizResultado3);
+
+console.log('matrizA => ', matrizA2);
+console.log('matrizB => ', matrizB2);
+console.log('matrizResultado (multiplicacion) => ', matrizResultado3);
 console.log(matrizA.length)
 
 // < Menor
