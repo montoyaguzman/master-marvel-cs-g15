6af24bc5924440b35041461e8d8a2bbc0002a93a
j.montoya <j.montoya@bbva.com>
2022-05-02 21:12:43 -0500

    docs: documentacion de funciones de arrow functions

--- 2-ecmascript/2-funciones-arrows.js
+++ 2-ecmascript/2-funciones-arrows.js
@@ -11,12 +11,33 @@ let resta = (num1, num2) => {
 }
 
 let saluda = name => {
-    console.log('Hola' + name)  
+    console.log('Hola' + name);
 }
 
 let sayGoodBye = name => console.log('Hasta mañana ' + name);  
+
+let eating = () => console.log('comiendo...')
 
 suma(5, 4);
 resta(5, 4);
 saluda('jose');
 sayGoodBye('ruben');
+eating();
+
+/** Syntaxis de las funciones flecha
+// Funcion flecha ordinaria (con muchos parametros y cuerpo)
+(param1, param2, paramN) => { //cuerpo de la funcion }
+
+// Funcion flecha con 1 solo param (no requiere parentesis en los parametros)
+param1 => { //cuerpo de la funcion }
+
+// Funcion flecha con una sola linea como retorno (no tiene cuerpo {})
+(param1, param2, paramN) => // result
+
+// Funcion flecha con 1 solo param y una sola linea como retorno (no tiene ni parantesis ni cuerpo)
+param1 => //result
+
+// Funcion flecha sin parametros
+() => //result
+
+*/
