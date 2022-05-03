d4cc60ba5d7e2b8bdd37c52417a0f46d02847263
j.montoya <j.montoya@bbva.com>
2022-05-02 19:37:43 -0500

    docs: agrega explicacion a la multiplicacion de matrices

--- 1-logica/1.4-codigo/calculadora-matrices/src/index.js
+++ 1-logica/1.4-codigo/calculadora-matrices/src/index.js
@@ -75,6 +75,9 @@ const matrizB2 = [
  * Reglas de la MULTIPLICACION de matrices
  * 1. matrizResultado[filasM1][columnasM2] 
  * 2. Solo se puede multiplicar M1 y M2 si columnasM1 === filasM2
+ * 3. matrizResultado[fila][columa] = m1[fila0][columna0] + m2[fila0][columna0] 
+ *                                  + m1[fila0][columna1] + m2[fila1][columna0]
+ *                                  + m1[fila0][columna2] + m2[fila2][columna0]
 */
 for (var fila = 0; fila < matrizResultado3.length; fila++) {
     for (var columna = 0; columna < matrizResultado3.length; columna++) {
@@ -82,9 +85,10 @@ for (var fila = 0; fila < matrizResultado3.length; fila++) {
         /**
          * Primer avance, tenia errores
          */
-        // matrizResultado3[fila][columna] = matrizResultado3[fila][columna] + matrizA2[fila][columna] * matrizB2[fila][columna]
+        matrizResultado3[fila][columna] = matrizResultado3[fila][columna] + matrizA2[fila][columna] * matrizB2[fila][columna]
         // matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][0] * matrizB[0][0]
-        // matrizResultado3[1][1] = matrizResultado3[1][1] + matrizA[1][1] * matrizB[1][1]
+        // matrizResultado3[0][1] = matrizResultado3[0][1] + matrizA[0][1] * matrizB[0][1]
+        // matrizResultado3[0][2] = matrizResultado3[0][2] + matrizA[0][2] * matrizB[0][2]
 
         /**
          * Pseudocodigo de correcion de errores
@@ -93,7 +97,10 @@ for (var fila = 0; fila < matrizResultado3.length; fila++) {
         // matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][1] * matrizB[1][0]
         // matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][2] * matrizB[2][0]
 
-        /** Solucion */
+        /** Solucion 
+         * La variable auxiliar la usamos para simular el pseudocodigo que hicimos arriba
+         * donde se va recorriendo la columna de la matrizA2 y la fila de la matrizB2.
+        */
         for (var aux = 0; aux < matrizResultado3.length; aux++) {
             matrizResultado3[fila][columna] = matrizResultado3[fila][columna] + matrizA2[fila][aux] * matrizB2[aux][columna];
         }
