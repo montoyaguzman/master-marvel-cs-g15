076d90db7150c995f21d9548237c18cbd6c11dfc
j.montoya <j.montoya@bbva.com>
2022-05-04 19:31:46 -0500

    feat: literal templates

---
+++ 2-ecmascript/10-literal-templates.js
@@ -0,0 +1,7 @@
+const superhero = { id: 1, name: 'peter', app: 'parker', alias: 'spiderman' };
+
+// Hola soy peter parker y yo soy spiderman
+console.log('Hola soy ' + superhero.name + ' ' + superhero.app + ' y yo soy ' + superhero.alias);
+console.log(`Hola soy ${superhero.name} ${superhero.app} y yo soy ${superhero.alias}`);
+// back ticks ``
+// literal template
