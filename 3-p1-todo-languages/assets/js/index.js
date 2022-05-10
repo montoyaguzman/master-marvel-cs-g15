f1dd5f8ea288b4146ac3edc243c7f28f15e28d07
j.montoya <j.montoya@bbva.com>
2022-05-09 21:15:56 -0500

    feat: agrega arreglo para languages

--- 3-p1-todo-languages/assets/js/index.js
+++ 3-p1-todo-languages/assets/js/index.js
@@ -1,15 +1,22 @@
 // const formElement = document.getElementById('language-form');
 const formElement = document.querySelector('#language-form'); // funciona con selectores tipo css
+let languages = [];
 
 formElement.addEventListener('submit', (event) => {
     event.preventDefault();
     // destructutring
-    const { language, status } = event.target;
+    const { languageElement, statusRadioElement } = event.target;
     // const language = event.target.language;
     // const status = event.target.status;
-    const languageValue = language.value;
-    const radiosNodeList = status;
+    const languageValue = languageElement.value;
+    const radiosNodeList = statusRadioElement;
     const checkedElement = Array.from(radiosNodeList).find(element => element.checked);
     const statusValue = checkedElement.value;
-    console.log(languageValue, statusValue);
+    const language = { 
+        text: languageValue, 
+        status: statusValue 
+    };
+    languages.push(language);
+    console.log(languages);
+
 });
