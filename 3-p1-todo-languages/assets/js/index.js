5787cd1597789227bc6e74382f0b8e8aac04a894
j.montoya <j.montoya@bbva.com>
2022-05-09 20:00:25 -0500

    feat: uso de addEventListener y las props de event

--- 3-p1-todo-languages/assets/js/index.js
+++ 3-p1-todo-languages/assets/js/index.js
@@ -1,1 +1,10 @@
-console.log('hola desde el js')
+// const formElement = document.getElementById('language-form');
+const formElement = document.querySelector('#language-form'); // funciona con selectores tipo css
+
+formElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    const inputData = event.target.language;
+    console.log(inputData);
+    console.log(inputData.value);
+});
