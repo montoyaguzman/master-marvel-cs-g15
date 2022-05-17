ba496026607ed7f05936ca9a698dea4df6995fae
j.montoya <j.montoya@bbva.com>
2022-05-16 20:02:23 -0500

    feat: agrega comparticion de metodos

---
+++ 5-prototypos-poo/3-compartir-metodos.js
@@ -0,0 +1,29 @@
+// compartir metodos
+const autoMethods = {
+    arranca: function(aceleracion) {
+      console.log('arranca...', aceleracion)
+    },
+    frena: function() {
+      console.log('arranca...')
+    }
+  }
+  
+  // funcion constructora
+  function createAuto(name, price, noLlantas, dimensiones, millas) {
+    const auto = {
+      name: name,
+      price: price,
+      noLlantas: noLlantas,
+      dimensiones: dimensiones,
+      millas: millas,
+      arranca: autoMethods.arranca,
+      frena: autoMethods.frena
+    }
+    return auto;
+  }
+  
+  const tsuru = createAuto('tsuru', 200, 4, '20x120x80', 100);
+  const mazda = createAuto('mazda', 400, 4, '30x130x90', 50);
+  
+  console.log(tsuru.arranca(10));
+  console.log(mazda.arranca(100));
